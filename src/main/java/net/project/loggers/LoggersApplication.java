57394485851356b17ae7f64a78d23cebb4d25082
Mohan Kumar N <mohankumar.n24@outlook.com>
2025-08-21 14:40:21 +0530

    Initial commit

--- src/main/java/net/project/loggers/LoggersApplication.java
+++ src/main/java/net/project/loggers/LoggersApplication.java
@@ -11,3 +11,20 @@ public class LoggersApplication {
 	}
 
 }
+
+/*
+14:38:11.094 [http-nio-8080-exec-1] ERROR net.project.loggers.controller.UserController - Controller: This is an ERROR level message
+14:38:11.094 [http-nio-8080-exec-1] WARN  net.project.loggers.controller.UserController - Controller: This is a WARN level message
+14:38:11.095 [http-nio-8080-exec-1] INFO  net.project.loggers.controller.UserController - Controller: This is an INFO level message
+14:38:11.095 [http-nio-8080-exec-1] DEBUG net.project.loggers.controller.UserController - Controller: This is a DEBUG level message
+14:38:11.095 [http-nio-8080-exec-1] TRACE net.project.loggers.controller.UserController - Controller: This is a TRACE level message
+
+14:38:11.095 [http-nio-8080-exec-1] ERROR net.project.loggers.service.UserService - Service: This is an ERROR level message
+14:38:11.095 [http-nio-8080-exec-1] WARN  net.project.loggers.service.UserService - Service: This is a WARN level message
+14:38:11.095 [http-nio-8080-exec-1] INFO  net.project.loggers.service.UserService - Service: This is an INFO level message
+14:38:11.095 [http-nio-8080-exec-1] DEBUG net.project.loggers.service.UserService - Service: This is a DEBUG level message
+
+14:38:11.095 [http-nio-8080-exec-1] ERROR net.project.loggers.repository.UserRepository - Repository: This is an ERROR level message
+14:38:11.095 [http-nio-8080-exec-1] WARN  net.project.loggers.repository.UserRepository - Repository: This is a WARN level message
+14:38:11.095 [http-nio-8080-exec-1] INFO  net.project.loggers.repository.UserRepository - Repository: This is an INFO level message
+*/
